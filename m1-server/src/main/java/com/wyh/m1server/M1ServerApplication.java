e76fc44a3d21dfd4c41ef93c95ec659dd986a301
王玉虎 <wangyuhu@xlauncher.io>
2021-09-10 15:24:25 +0800

    init java code

---
+++ m1-server/src/main/java/com/wyh/m1server/M1ServerApplication.java
@@ -0,0 +1,13 @@
+package com.wyh.m1server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M1ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M1ServerApplication.class, args);
+	}
+
+}
---
+++ m1-server/src/test/java/com/wyh/m1server/M1ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyh.m1server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M1ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
